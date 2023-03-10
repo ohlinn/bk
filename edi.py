3f10530cb73f8b698821a0d57a892231fd544b24
vainille <vainillee@outlook.com>
2023-03-10 01:23:05 -0600

    :D
    :D

---
+++ edi.py
@@ -0,0 +1,33 @@
+import json
+
+# SECTION vars
+f = open("board.json", "r")
+# returns JSON object as
+# a dictionary
+albums = json.load(f)
+accounts_ = []
+
+
+
+for account in albums["albums"][0]["accounts"]:
+    account_ = {}
+    account_.update(
+        {
+            "name": account["name"],
+            "code": account["code"],
+            "bar": account["bar"],
+            "percentage": account["percentage"],
+            "progress": account["progress"],
+            "total": account["total"],
+            "extra": account["extra"],
+            "missing": account["missing"],
+            "spares": account["spares"],
+            "sets": account["sets"],
+        }
+    )
+    accounts_.append(account_)
+albums["albums"][0].update({"accounts": accounts_})
+
+
+with open("neww.json", "w") as outfile:
+    json.dump(albums, outfile)
